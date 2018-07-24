b1d0fb05afc07e4b5b89eb9a2e4dd72e63c4ea95
Nitin <canvas.nv@gmail.com>
2018-07-24 09:38:08 +0530

    favourite dao made

---
+++ app/src/main/java/com/importio/nitin/popularmovies/Database/FavouriteDao.java
@@ -0,0 +1,24 @@
+package com.importio.nitin.popularmovies.Database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface FavouriteDao {
+
+    @Query("select * from favourite")
+    List<FavouriteEntry> getAllFavMovies();
+
+    @Insert
+    void insertFavMovie(FavouriteEntry movie);
+
+    @Delete
+    void removeFavMovie(FavouriteEntry movie);
+
+    @Query("select * from favourite where movieId=:id")
+    FavouriteEntry getFavById(int id);
+}
